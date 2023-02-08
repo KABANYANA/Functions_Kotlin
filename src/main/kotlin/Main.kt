dac4122b92a847ae929ed3e5950d717ee4371160
KABANYANA <joyeusekabanyana@gmail.com>
2023-02-08 07:21:56 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+//you may call different functions in the main function
+    println(add)
+}
+fun addition(num1:Int,num2:Int): Int{
+
+    var add = num1+num2
+    return add
+
+
+}
+fun greet(name:String){
+    println("hello,"+name)
+}
+
+val  add = addition(1,2)
+val  add2 = addition(10000,20000)
